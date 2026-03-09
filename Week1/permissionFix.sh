9afca77ab07d3add0eb9a31e57e717e29bf09afd
moeez7978-linux <moeez7978@outlook.com>
2026-03-09 16:28:45 +0500

    Made Script for checking permissions

---
+++ Week1/permissionFix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Directory
+DIR="/Downloads"
+#Permissions defiend by root user
+permissions = "750"
+#Checking for current permission of Directory
+current_perm=$(stat -c "%a" "$DIR")
+
+if [ "$current_perm" -eq 777 ]; then
+    chmod "$permissions" "$DIR"
+    echo "Permissions Fixed"
+else 
+    echo "Permissions are ok!!"
+fi
