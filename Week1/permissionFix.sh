475802d4f3009c417bb3ec02099ea4fac93991b0
moeez7978-linux <moeez7978@outlook.com>
2026-03-09 18:39:42 +0500

    Fixed Syntax Errors

--- Week1/permissionFix.sh
+++ Week1/permissionFix.sh
@@ -3,7 +3,7 @@
 #Directory
 DIR="/Downloads"
 #Permissions defiend by root user
-permissions = "750"
+permissions="750"
 #Checking for current permission of Directory
 current_perm=$(stat -c "%a" "$DIR")
 
--- Week1/serviceRestart.sh
+++ Week1/serviceRestart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-service = "nginx"
-memory = $(systemctl show "$service" --property=MemoryCurrent --value)
-maxlimit = 256*1024*1024 #256 MBs
+service="nginx"
+memory=$(systemctl show "$service" --property=MemoryCurrent --value)
+maxlimit=268435456 #256 MBs -> 256*1024*1024 
 
-if[ "$memory" -gt "$maxlimit" ];then
+if [ "$memory" -gt "$maxlimit" ]; then
    systemctl restart "$service"
    echo "Service Restarted Successfully!"
 else
