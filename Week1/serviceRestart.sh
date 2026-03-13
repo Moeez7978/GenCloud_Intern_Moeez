adc74c832ddca5ea524f2209818f03c859dc4f4c
moeez7978-linux <moeez7978@outlook.com>
2026-03-13 17:18:50 +0500

    Modified

--- Week1/serviceRestart.sh
+++ Week1/serviceRestart.sh
@@ -2,7 +2,7 @@
 
 service="nginx"
 memory=$(systemctl show "$service" --property=MemoryCurrent --value)
-maxlimit=268435456 #256 MBs -> 256*1024*1024 
+maxlimit=268435456      #256 MBs -> 256*1024*1024 
 
 if [ "$memory" -gt "$maxlimit" ]; then
    systemctl restart "$service"
