ba1a8725553dcb089a33f059fb461b2950ec6218
moeez7978-linux <moeez7978@outlook.com>
2026-03-16 19:01:44 +0500

    Next Week

--- Week1/permissionFix.sh
+++ Week1/permissionFix.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-
+recipient="moeez7978911@gmail.com"
 #Directory
-DIR="/home/moeez/Downloads2"
+DIR="/Downloads"
+
 #Permissions defiend by root user
 permissions="750"
 if [ -d "$DIR" ]; then
+
 #Checking for current permission of Directory
 current_perm=$(stat -c "%a" "$DIR")
+
  if [ "$current_perm" -eq 777 ]; then
+ 
+#Send mail to recipient that someone changed the directory permissions restoring them
+
     chmod "$permissions" "$DIR"
     echo "Permissions Fixed"
  else 
     echo "Permissions are ok!!"
  fi
+
 else
    echo "Directory Doesn,t Exists"
 fi
--- Week1/serviceRestart.sh
+++ Week1/serviceRestart.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+recipient="moeez7978911@gmail.com"
+
 service="nginx"
 memory=$(systemctl show "$service" --property=MemoryCurrent --value)
 maxlimit=268435456      #256 MBs -> 256*1024*1024 
 
 if [ "$memory" -gt "$maxlimit" ]; then
+
    systemctl restart "$service"
+# Send Mail to recipient that service excceeded the max memory limit and restarted successfully 
+
    echo "Service Restarted Successfully!"
+
 else
     echo "Service Running Properly"
 fi
